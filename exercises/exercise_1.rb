c1bd18192f2509982e82d7636acdf954046f8abb
Valentin <valentin.lyash@gmail.com>
2022-02-02 17:37:19 -0500

    commiting exercise 4 in ruby, filtering using CRUD Active Records (AR) .where functionality to find/fetch stores with explicit filtering criteria

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,7 @@ puts "----------"
 # Your code goes below here ...
 store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 # store1.count
-store2 = Store.create(name: "Richmond", annual_revenue: 12600000, mens_apparel: false, womens_apparel: "women's")
+store2 = Store.create(name: "Richmond", annual_revenue: 12600000, mens_apparel: false, womens_apparel: true)
 # store2.count
 
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store4 =Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6= Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores =Store.where(mens_apparel: true)
+
+@mens_stores.each do |mens_store|
+    puts "#{mens_store.name} & #{mens_store.annual_revenue}"
+end
+
+# @womens_stores = Store.where(womens_apparel: true, "annual_revenue > ?", 1000000)
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: 1000000..Float::INFINITY)
+
+@womens_stores.each do |womens_store|
+    puts "#{womens_store.name} & #{womens_store.annual_revenue}"
+end
