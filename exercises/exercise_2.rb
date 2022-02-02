2681e56fee6f03317baad83aeacb075cdcc68a30
Valentin <valentin.lyash@gmail.com>
2022-02-02 17:11:46 -0500

    implemented CRUD using ruby by reading/updating store 1 name to burnaby store, then updating store and then deleting store by id=3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+
 # Your code goes here ...
+
+@store1 = Store.find_by("id=1")
+@store2 = Store.find_by("id=2")
+@store1.update(name: "Burnaby store")
+puts "Store 1 name is: #{@store1.name}"
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
+# attr_accessor :name, :annual_revenue, :apparel
+
+# def initialize(name, annual_revenue, apparel) 
+#     @name= name
+#     @annual_revenue = annual_revenue
+#     @apparel = apparel
+# end
+
+# def count
+#     @count = count+1 
+# end
 
 end
