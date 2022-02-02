640c59b13b66d5628ddbe9f39558655b02f2a130
Valentin <valentin.lyash@gmail.com>
2022-02-02 17:55:34 -0500

    query stores with over 1M in revenue, total revenue from all stores, and average revenue per store

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# total_revenue = Store.calculate(:sum, annual_revenue)
+total_revenue = Store.sum("annual_revenue")
+total_stores = Store.count
+avg_revenue = total_revenue / total_stores
+
+puts "total revnue is: #{total_revenue}"
+puts "average revenue is: #{avg_revenue}"
+
+@largest_stores = Store.where( annual_revenue: 1000000..Float::INFINITY)
+
+number_stores = @largest_stores.count
+puts "number of stores with over $1M in revnue is: #{number_stores}"
